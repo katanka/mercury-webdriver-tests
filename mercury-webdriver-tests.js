fff16607928fa2136b2fbc82c6cbf425b495efc7
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-06-26 15:54:11 -0700

    style

--- mercury-webdriver-tests.js
+++ mercury-webdriver-tests.js
@@ -13,28 +13,28 @@ var currentTests = 0,
     testingStarted = false;
 
 startServer();
-waitFor(serverReady, "Server started.\n", "waiting...", startTests, 100);
-waitFor(testsDone, "Tests complete.\n", null, function() { killServer(); finish(); }, 1000)
+waitFor(serverReady, 'Server started.\n', 'waiting...', startTests, 100);
+waitFor(testsDone, 'Tests complete.\n', null, function() { killServer(); finish(); }, 1000)
 
 //Start the selenium
-function startServer(){
+function startServer() {
     console.log('Starting Selenium server...');
     exec('java -jar selenium-server-standalone-2.45.0.jar', {silent:true}, function(status, output){});
 }
 
 // Returns true if the Selenium server is ready
-function serverReady(){
+function serverReady() {
     return exec('netstat -anp tcp | awk \'$6 == "LISTEN" && $4 ~ "\.4444"\'', {silent:true}).output;
 }
 
 // when condition is true, execute callback
 function waitFor(condition, successMessage, waitMessage, callback, time) {
-    if (condition()){
+    if (condition()) {
         if (successMessage) {
             console.log(successMessage);
         }
         callback();
-    }else{
+    } else {
         if (waitMessage) {
             console.log(waitMessage);
         }
@@ -76,9 +76,10 @@ function testsDone() {
 function killServer() {
     // this must be terrible practice
     console.log("Stopping server.\n");
-    exec('kill \"$(ps aux | grep \'selenium\' | grep -v \'grep\' | head -n 1 | awk \'{print $2}\')\"');
+    exec('kill \"$(ps aux | grep \'selenium-server-standalone\' | grep -v \'grep\' | head -n 1 | awk \'{print $2}\')\"');
 }
 
+//Do stuff with the data we gathered
 function finish() {
     console.log('Out of ' + totalTests + ' tests, ' + passedTests + ' passed.');
 }
