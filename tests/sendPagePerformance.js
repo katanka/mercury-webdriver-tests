49e65655003219d5fe5b14109e40172fda3986c1
lizlux <liz@lizlux.net>
2015-07-30 17:27:20 -0700

    HG-816 adding test for page performance sender

---
+++ tests/sendPagePerformance.js
@@ -0,0 +1,53 @@
+module.exports = function(webdriverio, options, callback){
+	// TEST HERE
+	var passed = false,
+		title = 'sendPagePerformance',
+		message = '',
+		domain = require('domain').create();
+
+	domain.on('error', function(error) {
+		passed = false;
+		message = error.stack;
+	});
+
+	domain.add(webdriverio);
+
+	domain.run(function () {
+		try {
+			var client = webdriverio.remote(options);
+
+			client
+				.init()
+				// todo: make this configurable
+				.url('http://sandbox-s4.ja.destiny.wikia.com/wiki/Destiny?useskin=mercury&buckysampling=100&noexternals=1')
+				.waitUntil(function() {
+					return this.execute(function() {
+						return window.readyState === 'complete';
+					}).then(function(res) {
+						return res.value;
+					});
+				})
+				.pause(200)
+				.execute(function () {
+					return M.prop('pagePerformanceSent')
+				})
+				.then(function (returned) {
+					passed = returned.value === true;
+					if (!passed) {
+						message = 'Page performance stats not sent';
+					}
+				})
+				.end()
+				.then(function () {
+					callback({
+						'title': title,
+						'passed': passed,
+						'message': message
+					});
+				});
+		} catch (error) {
+			// do nothing
+		}
+	});
+
+};
